5e54b75c525924a830a676458c564e153f780251
Kieran Brownlees <kieran@mootium.co>
2016-04-10 17:04:49 +1200

    changeMemoize with some basic helper functions
    Inspired by https://github.com/reactjs/reselect/issues/71, a memoize
    function which will perform a callback everytime the result changes.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-export function add(x, y) {
-  return x + y;
+
+import { createSelectorCreator, defaultMemoize } from 'reselect';
+
+const notset = {};
+
+
+export function changeMemoize(func, changeCallback, memoize) {
+  const memoizeFunction = memoize || defaultMemoize;
+
+  const memoizeInstance = memoizeFunction(func);
+  if (changeCallback === undefined) {
+    return memoizeInstance;
+  }
+
+  let lastArgs = notset;
+  let lastResult = notset;
+  return (...args) => {
+    const result = memoizeInstance(...args);
+    if (result !== lastResult || lastResult === notset) {
+      changeCallback(lastArgs, lastResult, args, result);
+      lastResult = result;
+      lastArgs = args;
+    }
+
+    return result;
+  };
+}
+
+export function createSelectorWithChangeCallback(callback, ...args) {
+  return createSelectorCreator(changeMemoize, callback)(...args);
+}
+
+function logNamedChange(name) {
+  return (lastArgs, lastResult, newArgs, newResult) => {
+    // eslint-disable-next-line no-console
+    console.log(
+      name || 'unknown', '\n',
+      '\t', 'lastArgs:', lastArgs, '\n',
+      '\t', 'lastResult:', lastResult, '\n',
+      '\t', 'newArgs:', newArgs, '\n',
+      '\t', 'newResult:', newResult, '\n'
+    );
+  };
+}
+
+export function createSelector(...args) {
+  let name;
+  if (typeof(args[0]) === 'string') {
+    name = args.shift();
+  }
+
+  let changeCallback;
+  if (process.env.NODE_ENV !== 'production') {
+    changeCallback = logNamedChange(name);
+  }
+
+  return createSelectorWithChangeCallback(changeCallback, ...args);
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,46 @@
 import expect from 'expect';
-import { add } from '../src';
+import { createSelector, createSelectorWithChangeCallback } from '../src';
 
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+describe('createSelectorWithChangeCallback', () => {
+  it('Should callback when a value changes', () => {
+    let calls = 0;
+    const selector = createSelectorWithChangeCallback(
+      () => { calls += 1 },
+      (state) => state,
+      (state) => { // eslint-disable-line arrow-body-style
+        return { state };
+      }
+    );
+
+    let state = {};
+    selector(state);
+    expect(calls).toBe(1);
+    selector(state);
+    expect(calls).toBe(1);
+    state = { something: 'different' };
+    selector(state);
+    expect(calls).toBe(2);
+  });
+});
+
+describe('createSelector', () => {
+  it('Output for README', () => {
+    const selector1 = createSelector(
+      'An awesome selector',
+      (state) => state,
+      (state) => { // eslint-disable-line arrow-body-style
+        return { selector1: state };
+      }
+    );
+    const selector2 = createSelector(
+      'A second awesome selector which uses the first awesome selector',
+      selector1,
+      (state) => { // eslint-disable-line arrow-body-style
+        return { selector2: state };
+      }
+    );
+
+    selector2({ initial: 'state' });
+    selector1({ second: 'state' });
   });
 });
